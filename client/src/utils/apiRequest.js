3d0a6168cb05ea807fba2c02aee1f88312fed99c
Surace6063 <sureshthapa6063@gmail.com>
2025-11-04 18:39:59 +0545

    set authorization token to every api request

--- client/src/utils/apiRequest.js
+++ client/src/utils/apiRequest.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { useUserStore } from "../zustand/userStore"
 
 const BASE_URL = "http://127.0.0.1:8000/api"
 
@@ -6,3 +7,17 @@ const BASE_URL = "http://127.0.0.1:8000/api"
 export const apiRequest = axios.create({
     baseURL: BASE_URL
 })
+
+// attach access token automatically
+apiRequest.interceptors.request.use(
+  (config) => {
+    // correct way to get Zustand store data outside React components
+    const { user } = useUserStore.getState();
+
+    if (user?.access) {
+      config.headers.Authorization = `Bearer ${user.access}`;
+    }
+
+    return config;
+  }
+);
